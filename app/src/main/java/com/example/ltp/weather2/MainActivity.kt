cff8223bf0e48902d1c82e7375e5534c2e5701c6
cyliong <liongtheprogrammer@gmail.com>
2021-09-11 22:45:35 +0800

    Remove unnecessary comments

--- app/src/main/java/com/example/ltp/weather2/MainActivity.kt
+++ app/src/main/java/com/example/ltp/weather2/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Weather2Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
