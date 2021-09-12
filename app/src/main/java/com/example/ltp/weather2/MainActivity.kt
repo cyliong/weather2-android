b3333ee4978deb38dc57bd9e3f22f4b4f1682867
cyliong <liongtheprogrammer@gmail.com>
2021-09-12 23:16:24 +0800

    Create MainScreen composable and preview

--- app/src/main/java/com/example/ltp/weather2/MainActivity.kt
+++ app/src/main/java/com/example/ltp/weather2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ltp.weather2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.ltp.weather2.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +13,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-
+                MainScreen()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Text("No weather data")
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    AppTheme {
+        MainScreen()
     }
 }
