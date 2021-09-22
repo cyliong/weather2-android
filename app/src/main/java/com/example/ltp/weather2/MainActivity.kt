b641c2bb6ec53c8dc1b3fd82a06a054bf38c5605
cyliong <liongtheprogrammer@gmail.com>
2021-09-22 17:54:01 +0800

    Set the Search IME action

--- app/src/main/java/com/example/ltp/weather2/MainActivity.kt
+++ app/src/main/java/com/example/ltp/weather2/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ltp.weather2.ui.theme.AppTheme
 
@@ -44,6 +46,7 @@ fun MainScreen() {
                 onValueChange = { },
                 modifier = Modifier.fillMaxWidth(),
                 placeholder = { Text("Search a city") },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
                 trailingIcon = {
                     IconButton(onClick = { }) {
                         Icon(
