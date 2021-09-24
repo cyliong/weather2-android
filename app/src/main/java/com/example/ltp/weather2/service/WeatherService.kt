d7ceb59f7fbf5569cc20346b3e2881b6c5382027
cyliong <liongtheprogrammer@gmail.com>
2021-09-24 21:31:58 +0800

    Create the WeatherService class

---
+++ app/src/main/java/com/example/ltp/weather2/service/WeatherService.kt
@@ -0,0 +1,11 @@
+package com.example.ltp.weather2.service
+
+import com.example.ltp.weather2.model.Weather
+
+class WeatherService {
+
+    fun getWeather(cityName: String): Weather? {
+        return null
+    }
+
+}
