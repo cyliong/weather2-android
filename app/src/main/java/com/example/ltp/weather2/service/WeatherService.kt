b5f6ddf79f6fd74ed8bc401cc47a5c49f4cf4770
cyliong <liongtheprogrammer@gmail.com>
2021-09-26 22:09:36 +0800

    Implement the getWeather() method

--- app/src/main/java/com/example/ltp/weather2/service/WeatherService.kt
+++ app/src/main/java/com/example/ltp/weather2/service/WeatherService.kt
@@ -1,11 +1,61 @@
 package com.example.ltp.weather2.service
 
+import android.util.Log
 import com.example.ltp.weather2.model.Weather
+import org.json.JSONObject
+import java.io.IOException
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+private const val API_KEY = ""
+private const val SERVICE_URL =
+    "https://api.openweathermap.org/data/2.5/weather?appid=$API_KEY&units=metric"
+private const val TAG = "WeatherService"
 
 class WeatherService {
 
     fun getWeather(cityName: String): Weather? {
+        try {
+            val jsonObject = getServiceResponse(cityName)
+
+            val mainObject = jsonObject.getJSONObject("main")
+            val temperature = mainObject.getDouble("temp")
+            val humidity = mainObject.getInt("humidity")
+
+            val weatherObject = jsonObject.getJSONArray("weather").getJSONObject(0)
+            val condition = weatherObject.getString("main")
+
+            val city = jsonObject.getString("name")
+
+            val systemObject = jsonObject.getJSONObject("sys")
+            val countryCode = systemObject.getString("country")
+
+            Log.v(TAG, "Weather - $city, $countryCode: $temperature°C, $humidity%, $condition.")
+
+            return Weather(city, countryCode, temperature, humidity, condition)
+        } catch (e: IOException) {
+            Log.e(TAG, e.toString())
+        }
         return null
+    }
+
+    private fun getServiceResponse(cityName: String): JSONObject {
+        val urlString = "$SERVICE_URL&q=$cityName"
+        Log.v(TAG, "URL: $urlString")
+
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpsURLConnection
+
+        var data = connection.inputStream.read()
+        val builder = StringBuilder()
+
+        while (data != -1) {
+            builder.append(data.toChar())
+            data = connection.inputStream.read()
+        }
+        val json = builder.toString()
+        Log.v(TAG, "JSON: $json")
+        return JSONObject(json)
     }
 
 }
