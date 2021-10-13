6947c1b2553c1ecfce3a13b0e9cc640191eafa0e
cyliong <liongtheprogrammer@gmail.com>
2021-10-13 22:42:24 +0800

    Make data class parcelable

--- app/src/main/java/com/example/ltp/weather2/model/Weather.kt
+++ app/src/main/java/com/example/ltp/weather2/model/Weather.kt
@@ -1,9 +1,13 @@
 package com.example.ltp.weather2.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Weather(
     val city: String,
     val countryCode: String,
     val temperature: Double,
     val humidity: Int,
     val condition: String
-)
+) : Parcelable
